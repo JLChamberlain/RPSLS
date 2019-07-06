faefcb71f345c477c22601213047d38c8a6043c9
JLChamberlain <joshuachamberlain42@hotmail.com>
2019-07-06 10:21:45 +0100

    Fixed Console.log & hover issue

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ let faviconElem = document.getElementById('favicon');
 faviconElem.setAttribute('href', `IMG/Favicons/favicon${Math.floor(5 * Math.random()) + 1}.ico`);
 
 // Start of the Javascript for the actual game
-const choices = document.querySelectorAll('.RPSLS');
+const choices = document.querySelectorAll('.RPSLS > div');
 const score = document.getElementById('score');
 const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
 
 const scoreboard = {
-    player: 0,
-    computer: 0
-}
+	player: 0,
+	computer: 0,
+};
 
-// Playing the game 
+// Playing the game
+
 function play(e) {
-    console.log(e.target.id);
+	console.log(e.target.parentElement.parentElement.id);
 }
 
-// Event Listeners 
-choices.forEach(choice => choice.addEventListener('click', play));
+// Event Listeners
+choices.forEach((choice) => choice.addEventListener('click', play));
